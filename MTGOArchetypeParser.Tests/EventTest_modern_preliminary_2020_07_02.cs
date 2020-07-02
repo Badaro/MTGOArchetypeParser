fdf26e935b3f1d650387b5a8629b4ad00a972749
Filipe Donghia Badaro <badaro@gmail.com>
2020-07-02 13:13:43 -0300

    Added 2020-07-02 prelim

---
+++ MTGOArchetypeParser.Tests/EventTest_modern_preliminary_2020_07_02.cs
@@ -0,0 +1,147 @@
+using FluentAssertions;
+using MTGOArchetypeParser.Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTGOArchetypeParser.Tests
+{
+    public class EventTest_modern_preliminary_2020_07_02 : EventTest
+    {
+        [Test]
+        public void Deck01_Ouranos139_PrimevalTitan_AmuletTitan_WUBRG()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck01_Ouranos139_PrimevalTitan_AmuletTitan_WUBRG(),
+                ArchetypeColor.WUBRG,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.PrimevalTitan),
+                typeof(MTGOArchetypeParser.Archetypes.Modern.AmuletTitan),
+                null
+            );
+        }
+
+        [Test]
+        public void Deck02_Toastxp_SnowControl_SultaiSnow_UBG()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck02_Toastxp_SnowControl_SultaiSnow_UBG(),
+                ArchetypeColor.UBG,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.SnowControl),
+                typeof(MTGOArchetypeParser.Archetypes.Modern.SultaiSnow),
+                null
+            );
+        }
+
+        [Test]
+        public void Deck03_fingers1991_SnowControl_WUG()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck03_fingers1991_SnowControl_WUG(),
+                ArchetypeColor.WUG,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.SnowControl),
+                null,
+                null
+            );
+        }
+
+        [Test]
+        public void Deck04_TheoJung_SnowControl_SultaiSnow_UBG()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck04_TheoJung_SnowControl_SultaiSnow_UBG(),
+                ArchetypeColor.UBG,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.SnowControl),
+                typeof(MTGOArchetypeParser.Archetypes.Modern.SultaiSnow),
+                null
+            );
+        }
+
+        [Test]
+        public void Deck05_SamcasterMage_ETron_C()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck05_SamcasterMage_ETron_C(),
+                ArchetypeColor.C,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.ETron),
+                null,
+                null
+            );
+        }
+
+        [Test]
+        public void Deck06_Icteridae_ETron_C()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck06_Icteridae_ETron_C(),
+                ArchetypeColor.C,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.ETron),
+                null,
+                null
+            );
+        }
+
+        [Test]
+        public void Deck07_TubbyBatman_Prowess_UR()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck07_TubbyBatman_Prowess_UR(),
+                ArchetypeColor.UR,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.Prowess),
+                null,
+                null
+            );
+        }
+
+        [Test]
+        public void Deck08_nolyfe_Prowess_R()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck08_nolyfe_Prowess_R(),
+                ArchetypeColor.R,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.Prowess),
+                null,
+                null
+            );
+        }
+
+        [Test]
+        public void Deck09_karatedom_Goblins_SnoopGoblins_BR()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck09_karatedom_Goblins_SnoopGoblins_BR(),
+                ArchetypeColor.BR,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.Goblins),
+                typeof(MTGOArchetypeParser.Archetypes.Modern.SnoopGoblins),
+                null
+            );
+        }
+
+        [Test]
+        public void Deck10_MrSheep_Burn_WR()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck10_MrSheep_Burn_WR(),
+                ArchetypeColor.WR,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.Burn),
+                null,
+                null
+            );
+        }
+
+        [Test]
+        public void Deck11_Motown123_Merfolk_U()
+        {
+            Test(
+                new MTGOArchetypeParser.Tests.SampleData.modern_preliminary_2020_07_02.Deck11_Motown123_Merfolk_U(),
+                ArchetypeColor.U,
+                typeof(MTGOArchetypeParser.Archetypes.Modern.Merfolk),
+                null,
+                null
+            );
+        }
+
+
+    }
+}
