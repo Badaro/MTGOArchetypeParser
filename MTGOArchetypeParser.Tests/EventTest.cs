570969bdb4e6f0b254061f9438b36619d589ba9e
Filipe Donghia Badaro <badaro@gmail.com>
2020-07-07 23:24:09 -0300

    Slight cleanup of test code

--- MTGOArchetypeParser.Tests/EventTest.cs
+++ MTGOArchetypeParser.Tests/EventTest.cs
@@ -13,27 +13,17 @@ namespace MTGOArchetypeParser.Tests
     public class EventTest
     {
 
-        protected void Test(ISampleDeck deck, ArchetypeColor? expectedColor = null, Type expectedArchetype = null, Type expectedVariant = null, ArchetypeCompanion? expectedCompanion = null)
+        protected void Test(ISampleDeck deck, ArchetypeColor? expectedColor, Type expectedArchetype, Type expectedVariant = null, ArchetypeCompanion? expectedCompanion = null)
         {
             var result = ArchetypeAnalyzer.Detect(deck.Mainboard.Select(c => c.Name).ToArray(), deck.Sideboard.Select(c => c.Name).ToArray(), MTGOArchetypeParser.Archetypes.Modern.Loader.GetArchetypes());
 
-            result.Matches.Length.Should().Be(1);
+            result.Matches.Should().HaveCount(1);
 
-            if (expectedColor != null) result.Color.Should().Be(expectedColor);
-            else result.Color.Should().Be(ArchetypeColor.C);
+            result.Color.Should().Be(expectedColor);
+            result.Matches.First().Archetype.Should().BeOfType(expectedArchetype);
 
-            if (expectedArchetype != null)
-            {
-                result.Matches.Should().HaveCount(1);
-                result.Matches.First().Archetype.Should().BeOfType(expectedArchetype);
-
-                if (expectedVariant != null) result.Matches.First().Variant.Should().BeOfType(expectedVariant);
-                else result.Matches.First().Variant.Should().BeNull();
-            }
-            else
-            {
-                result.Matches.Should().HaveCount(0);
-            }
+            if (expectedVariant != null) result.Matches.First().Variant.Should().BeOfType(expectedVariant);
+            else result.Matches.First().Variant.Should().BeNull();
 
             if (expectedCompanion != null) result.Companion.Should().Be(expectedCompanion.Value);
             else result.Companion.Should().BeNull();
