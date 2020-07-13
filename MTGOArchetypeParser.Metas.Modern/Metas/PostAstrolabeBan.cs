59190d5bb68e73ef8b7e89100e8ecdffe8cfc9eb
Filipe Donghia Badaro <badaro@gmail.com>
2020-07-13 12:23:06 -0300

    Added post-astrolabe ban meta

---
+++ MTGOArchetypeParser.Metas.Modern/Metas/PostAstrolabeBan.cs
@@ -0,0 +1,15 @@
+﻿using MTGOArchetypeParser.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTGOArchetypeParser.Metas.Modern
+{
+    public class PostAstrolabeBan : ArchetypeMeta
+    {
+        public PostAstrolabeBan()
+        {
+            this.StartDate = new DateTime(2020, 07, 15, 00, 00, 00, DateTimeKind.Utc);
+        }
+    }
+}
